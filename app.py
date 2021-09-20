fa365f035034b134a189adb7dea1a75d90171f31
manuel <mesfardales@protonmail.com>
2021-09-19 21:48:35 -0500

    Nueva modificacion

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 import requests
+from urllib.parse import quote_plus 
 
-url = 'http://www.webscrapingfordatascience.com/basichttp/' 
-r = requests.get(url)
+url = 'http://www.webscrapingfordatascience.com/paramhttp/' 
+raw_string = "File System"
+parameter = {
+    'query' : 'esto es una prueba'
+}
+r = requests.get(url, params=parameter)
 print(r.text)
